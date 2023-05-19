a12d5ba376604b5036a7651a36cadedc07782308
Asaduzzaman Abir <anonnaabir00@gmail.com>
2023-05-20 01:25:48 +0600

    Initial Commit

---
+++ class-wp-floatingform.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       WP Floating Form
+ * Plugin URI:        https://stacksamurai.com/
+ * Description:       Simple floating form plugin for WordPress.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Stack Samurai
+ * Author URI:        https://stacksamurai.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-floatingform
+ * Domain Path:       /languages
+ */
