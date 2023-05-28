45c797988e9942b167c167c6189a740312a5f635
Asaduzzaman Abir <anonnaabir00@gmail.com>
2023-05-28 12:44:05 +0600

    Added

---
+++ class-awesome-floatingform.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Plugin Name:       Awesome Floating Form
+ * Plugin URI:        https://stacksamurai.com/
+ * Description:       Simple floating form plugin for WordPress.
+ * Version:           1.1
+ * Requires at least: 4.6
+ * Requires PHP:      7.2
+ * Tested up to:      6.2.2
+ * Author:            Stack Samurai
+ * Author URI:        https://stacksamurai.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       awesome-floatingform
+ * Domain Path:       /languages
+ */
+
+
+// Exit if accessed directly.
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Define AWESOME_FLOATINGFORM_PLUGIN_FILE.
+
+if ( ! defined( 'AWESOME_FLOATINGFORM_PLUGIN_FILE' ) ) {
+    define( 'AWESOME_FLOATINGFORM_PLUGIN_FILE', __FILE__ );
+}
+
+define('AWESOME_FLOATINGFORM_TEXTDOMAIN', 'awesome-floatingform');
+
+// Include the main WP_FloatingForm class.
+
+if ( ! class_exists( 'Awesome_FloatingForm' ) ) {
+    include_once dirname( __FILE__ ) . '/includes/init.php';
+}
